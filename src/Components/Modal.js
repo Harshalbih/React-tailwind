53b6b708e17515dc12d9d4c668c9d956f90ff1c3
Harshal <bihadeharsh@gmail.com>
2024-02-03 01:55:30 +0530

    Model complete

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,13 +1,19 @@
 import  ReactDOM  from "react-dom";
-function Modal (){
+function Modal ({onClose, children, actionBar}){
     
     
         return ReactDOM.createPortal(
             <div>
-        <div className="absolute  inset-0 bg-gray-300 opacity-80">
-        <div className="absolute inset-40 p-10 bg-white">It's a modal!</div></div>
+        <div onClick={onClose} className="fixed  inset-0 bg-gray-300 opacity-80">
+        <div className="absolute inset-40 max-h-max p-10 bg-white" id="modal">
+            <div className=" flex flex-col -justify-between h-full">
+            {children}
+            <div className="flex justify-end">{actionBar}</div>
+            </div>
+            </div>
+        </div>
         </div>,
-        document.querySelector('modal-container')
+        document.querySelector('.modal-container')
     )
 }
 
--- src/pages/Modalpage.js
+++ src/pages/Modalpage.js
@@ -10,11 +10,23 @@ function Modalpage() {
     const handleClick = () =>{
         setShowModal(true);
     }
+ 
+    const handleClose = ()=>{
+      setShowModal(false);
+    }
+
+    const modal = <Modal onClose={handleClose} actionBar={<Button primary >I accept</Button>}>
+      <p>
+        Please accept terms and conditions The website you are visiting is protected and accelerated by Incapsula. Your computer may have been infected by malware and therefore flagged by the Incapsula network. Incapsula displays this page for you to verify that an actual human is the source of the traffic to this site, and not malicious software.
+        Just click the I'm not a robot checkbox to pass the security check. Incapsula will remember you and will not show this page again. We recommend you run a virus and malware scan on your computer to remove any infection.
+      </p>
+    </Modal>
+
   return (
     <div className='relative'>
         <Sidebar />
         <Button onClick={handleClick} primary>Open Modal</Button>
-        {showModal && <Modal />}
+        {showModal && modal}
         </div>
   )
 }
