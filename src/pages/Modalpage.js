27099e82e4e83ef3cfb7c866dd8c13af854d1b99
Harshal <bihadeharsh@gmail.com>
2024-02-02 19:11:17 +0530

    Modal.js

--- src/pages/Modalpage.js
+++ src/pages/Modalpage.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Modal from '../Components/Modal'
 import Sidebar from '../Components/Sidebar';
+import { useState } from 'react';
+import Button from '../Components/Button';
 
 function Modalpage() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () =>{
+        setShowModal(true);
+    }
   return (
-    <div>
+    <div className='relative'>
         <Sidebar />
-        <Modal />
+        <Button onClick={handleClick} primary>Open Modal</Button>
+        {showModal && <Modal />}
         </div>
   )
 }
