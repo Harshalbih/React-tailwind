013d080cee7b1e9b594099a511a9b7d78c89330a
Harshal <bihadeharsh@gmail.com>
2024-02-04 02:15:17 +0530

    stacked

--- src/pages/Modalpage.js
+++ src/pages/Modalpage.js
@@ -18,7 +18,7 @@ function Modalpage() {
     const modal = <Modal onClose={handleClose} actionBar={<Button primary >I accept</Button>}>
       <p>
         Please accept terms and conditions The website you are visiting is protected and accelerated by Incapsula. Your computer may have been infected by malware and therefore flagged by the Incapsula network. Incapsula displays this page for you to verify that an actual human is the source of the traffic to this site, and not malicious software.
-        Just click the I'm not a robot checkbox to pass the security check. Incapsula will remember you and will not show this page again. We recommend you run a virus and malware scan on your computer to remove any infection.
+        Just click the I'm not a robot checkbox to pass the security check.  to pass the security check. Incapsula will remember you and will not show this page again. We recommend you run a virus and malware scan on your computer to remove any infection.
       </p>
     </Modal>
 
